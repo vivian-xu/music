0732b46b4d1088102f97c26e95a4af5387f1958d
vivian <vivian.zxx53@gmail.com>
2015-12-20 13:04:12 +0800

    style

--- public/js/site.js
+++ public/js/site.js
@@ -2,8 +2,8 @@
 function addEvent(obj, type, fn){ // type是无 on 的事件
     if(window.addEventListener){
         // Chrome,FireFox,Opera,Safari,IE9.0及以上
-        obj.addEventListener(type, fn, false);}
-    else{ 
+        obj.addEventListener(type, fn, false);
+    } else { 
         // IE8.0及以下
         obj.attachEvent('on'+type, fn);
     }
@@ -11,13 +11,21 @@ function addEvent(obj, type, fn){ // type是无 on 的事件
 
 // mousedown 时背景色改变
 function mdbg() {
-    var $this = $(this);
-    $this.css("background-color","#eee");
+    var me = $(this);
+    if( me.hasClass("blackKey")){
+        me.css("background-color","#eee");
+    } else {
+        me.css("background-color","#60b044")
+    }
 }
 
 function mubgTime() {
-    var $this = $(this);
-    setTimeout(function(){$this.css("background-color", "#000");}, 300);
+    var me = $(this);
+    if( me.hasClass("blackKey")){
+        setTimeout(function(){me.css("background-color", "#000");}, 300);
+    } else {
+        setTimeout(function(){me.css("background-color", "#fff");}, 300);
+    }
 }
 
 // ready 执行完后会销毁，将mubg,mubgTime放在里面，只将 mubgTime 函数绑定到了 mouseup 事件，但 mubg 未绑定（在mubgTime 中只以自付存在，当用时才去调用 mubg 函数）
@@ -27,19 +35,11 @@ $(document).ready(function(){
     // 给每个黑色键添加变色事件 
     black.each( function(){
         addEvent(this, "mousedown", mdbg);
-        addEvent(this, "mouseup", mubgTime);
-        
-    })
- 
- /*   black.bind({
-        mousedown:function(){
-        var $this = $(this);
-        console.log($this.nodeName);
-        $this.css("background-color","#eee");
-        },
-        mouseup:function(){
-        var $this = $(this);
-        $this.css("background-color","#000");}
+        addEvent(this, "mouseup", mubgTime);        
+    });
     
-    }); */
+    white.each( function(){
+        addEvent(this, "mousedown", mdbg);
+        addEvent(this, "mouseup", mubgTime);        
+    });
 })
